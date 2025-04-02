279209712eb36522af86546683ec403d7f0f7274
Drinne Divera <122563149+WhiteKarino@users.noreply.github.com>
2025-04-02 15:17:28 +0800

    Add files via upload

---
+++ Login/JwtAuthDemo/Models/RegisterModel.cs
@@ -0,0 +1,9 @@
+public class RegisterModel
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Address { get; set; }
+    public required string Phone { get; set; }
+}
